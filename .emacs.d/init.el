206963f9fe1afc4c13df21bc38da13c75c60f801
Jason Sakellariou <js.sakel@gmail.com>
2021-03-17 17:00:42 +0200

    Add ido-vertical, ibuffer and smex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,7 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 ;; -------------------------------------------------------------------------------- 
-;; Custom set variables
+;; custom set variables
 ;; -------------------------------------------------------------------------------- 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -33,7 +33,7 @@
  '(spacemacs-theme-underline-parens nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; if you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 190 :width normal :foundry "nil" :family "JetBrainsMono Nerd Font Mono")))))
