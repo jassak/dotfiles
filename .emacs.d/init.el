218cbc3f7014e5b838cc45df3a7db6d6ead729df
Jason Sakellariou <js.sakel@gmail.com>
2021-04-02 15:47:08 +0300

    Lots of changes in config.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,3 @@
-;; Straight package manager (https://github.com/raxod502/straight.el)
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(setq package-enable-at-startup nil)
-(straight-use-package 'use-package)
-
 ;; Load config file
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
@@ -31,9 +15,3 @@
  '(spacemacs-theme-comment-italic nil)
  '(spacemacs-theme-keyword-italic t)
  '(spacemacs-theme-underline-parens nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; if you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 190 :width normal :foundry "nil" :family "JetBrainsMono Nerd Font Mono")))))
