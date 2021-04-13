47d01e181b258acb192e4f954ceae8282b4fd7cc
Jason Sakellariou <js.sakel@gmail.com>
2021-04-13 07:42:09 +0300

    Update some stuff here and there

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,308 @@
 -- Required for hs (hammerspoon in the terminal)
 require("hs.ipc")
-require("winman")
-require("mocha")
+-- require("slowq")
+-- require("collage")
 
+hs.hotkey.alertDuration = 0
+hs.hints.showTitleThresh = 0
+hs.window.animationDuration = 0
 
+-- Use the standardized config location, if present
+custom_config = hs.fs.pathToAbsolute(os.getenv("HOME") .. '/.config/hammerspoon/private/config.lua')
+if custom_config then
+    print("Loading custom config")
+    dofile( os.getenv("HOME") .. "/.config/hammerspoon/private/config.lua")
+    privatepath = hs.fs.pathToAbsolute(hs.configdir .. '/private/config.lua')
+    if privatepath then
+        hs.alert("You have config in both .config/hammerspoon and .hammerspoon/private.\nThe .config/hammerspoon one will be used.")
+    end
+else
+    -- otherwise fallback to 'classic' location.
+    if not privatepath then
+        privatepath = hs.fs.pathToAbsolute(hs.configdir .. '/private')
+        -- Create `~/.hammerspoon/private` directory if not exists.
+        hs.fs.mkdir(hs.configdir .. '/private')
+    end
+    privateconf = hs.fs.pathToAbsolute(hs.configdir .. '/private/config.lua')
+    if privateconf then
+        -- Load awesomeconfig file if exists
+        require('private/config')
+    end
+end
+
+hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
+if string.len(hsreload_keys[2]) > 0 then
+    hs.hotkey.bind(hsreload_keys[1], hsreload_keys[2], "Reload Configuration", function() hs.reload() end)
+end
+
+-- ModalMgr Spoon must be loaded explicitly, because this repository heavily relies upon it.
+hs.loadSpoon("ModalMgr")
+
+-- Define default Spoons which will be loaded later
+if not hspoon_list then
+    hspoon_list = {
+        "WinWin",
+        -- "FnMate",
+    }
+end
+
+-- Load those Spoons
+for _, v in pairs(hspoon_list) do
+    hs.loadSpoon(v)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Then we create/register all kinds of modal keybindings environments.
+----------------------------------------------------------------------------------------------------
+-- Register windowHints (Register a keybinding which is NOT modal environment with modal supervisor)
+hswhints_keys = hswhints_keys or {"alt", "tab"}
+if string.len(hswhints_keys[2]) > 0 then
+    spoon.ModalMgr.supervisor:bind(hswhints_keys[1], hswhints_keys[2], 'Show Window Hints', function()
+        spoon.ModalMgr:deactivateAll()
+        hs.hints.windowHints()
+    end)
+end
+
+----------------------------------------------------------------------------------------------------
+-- appM modal environment
+spoon.ModalMgr:new("appM")
+local cmodal = spoon.ModalMgr.modal_list["appM"]
+cmodal:bind('', 'escape', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
+cmodal:bind('', 'Q', 'Deactivate appM', function() spoon.ModalMgr:deactivate({"appM"}) end)
+cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+if not hsapp_list then
+    hsapp_list = {
+        {key = 'f', name = 'Finder'},
+        {key = 's', name = 'Safari'},
+        {key = 't', name = 'Terminal'},
+        {key = 'v', id = 'com.apple.ActivityMonitor'},
+        {key = 'y', id = 'com.apple.systempreferences'},
+    }
+end
+for _, v in ipairs(hsapp_list) do
+    if v.id then
+        local located_name = hs.application.nameForBundleID(v.id)
+        if located_name then
+            cmodal:bind('', v.key, located_name, function()
+                hs.application.launchOrFocusByBundleID(v.id)
+                spoon.ModalMgr:deactivate({"appM"})
+            end)
+        end
+    elseif v.name then
+        cmodal:bind('', v.key, v.name, function()
+            hs.application.launchOrFocus(v.name)
+            spoon.ModalMgr:deactivate({"appM"})
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- clipshowM modal environment
+if spoon.ClipShow then
+    spoon.ModalMgr:new("clipshowM")
+    local cmodal = spoon.ModalMgr.modal_list["clipshowM"]
+    cmodal:bind('', 'escape', 'Deactivate clipshowM', function()
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'Q', 'Deactivate clipshowM', function()
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'N', 'Save this Session', function()
+        spoon.ClipShow:saveToSession()
+    end)
+    cmodal:bind('', 'R', 'Restore last Session', function()
+        spoon.ClipShow:restoreLastSession()
+    end)
+    cmodal:bind('', 'B', 'Open in Browser', function()
+        spoon.ClipShow:openInBrowserWithRef()
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'S', 'Search with Bing', function()
+        spoon.ClipShow:openInBrowserWithRef("https://www.bing.com/search?q=")
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'M', 'Open in MacVim', function()
+        spoon.ClipShow:openWithCommand("/usr/local/bin/mvim")
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'F', 'Save to Desktop', function()
+        spoon.ClipShow:saveToFile()
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'H', 'Search in Github', function()
+        spoon.ClipShow:openInBrowserWithRef("https://github.com/search?q=")
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'G', 'Search with Google', function()
+        spoon.ClipShow:openInBrowserWithRef("https://www.google.com/search?q=")
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+    cmodal:bind('', 'L', 'Open in Sublime Text', function()
+        spoon.ClipShow:openWithCommand("/usr/local/bin/subl")
+        spoon.ClipShow:toggleShow()
+        spoon.ModalMgr:deactivate({"clipshowM"})
+    end)
+
+    -- Register clipshowM with modal supervisor
+    hsclipsM_keys = hsclipsM_keys or {"alt", "C"}
+    if string.len(hsclipsM_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hsclipsM_keys[1], hsclipsM_keys[2], "Enter clipshowM Environment", function()
+            -- We need to take action upon hsclipsM_keys is pressed, since pressing another key to showing ClipShow panel is redundant.
+            spoon.ClipShow:toggleShow()
+            -- Need a little trick here. Since the content type of system clipboard may be "URL", in which case we don't need to activate clipshowM.
+            if spoon.ClipShow.canvas:isShowing() then
+                spoon.ModalMgr:deactivateAll()
+                spoon.ModalMgr:activate({"clipshowM"})
+            end
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- countdownM modal environment
+if spoon.CountDown then
+    spoon.ModalMgr:new("countdownM")
+    local cmodal = spoon.ModalMgr.modal_list["countdownM"]
+    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+    cmodal:bind('', '0', '5 Minutes Countdown', function()
+        spoon.CountDown:startFor(5)
+        spoon.ModalMgr:deactivate({"countdownM"})
+    end)
+    for i = 1, 9 do
+        cmodal:bind('', tostring(i), string.format("%s Minutes Countdown", 10 * i), function()
+            spoon.CountDown:startFor(10 * i)
+            spoon.ModalMgr:deactivate({"countdownM"})
+        end)
+    end
+    cmodal:bind('', 'return', '25 Minutes Countdown', function()
+        spoon.CountDown:startFor(25)
+        spoon.ModalMgr:deactivate({"countdownM"})
+    end)
+    cmodal:bind('', 'space', 'Pause/Resume CountDown', function()
+        spoon.CountDown:pauseOrResume()
+        spoon.ModalMgr:deactivate({"countdownM"})
+    end)
+
+    -- Register countdownM with modal supervisor
+    hscountdM_keys = hscountdM_keys or {"alt", "I"}
+    if string.len(hscountdM_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hscountdM_keys[1], hscountdM_keys[2], "Enter countdownM Environment", function()
+            spoon.ModalMgr:deactivateAll()
+            -- Show the keybindings cheatsheet once countdownM is activated
+            spoon.ModalMgr:activate({"countdownM"}, "#FF6347", true)
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- resizeM modal environment
+if spoon.WinWin then
+    spoon.ModalMgr:new("resizeM")
+    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+    cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
+    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
+    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
+    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
+    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
+    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
+    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
+    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
+    cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
+    cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
+    cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
+    cmodal:bind('', 'J', 'Downhalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
+    cmodal:bind('', 'Y', 'NorthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
+    cmodal:bind('', 'O', 'NorthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
+    cmodal:bind('', 'U', 'SouthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
+    cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
+    cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
+    cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
+    cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
+    cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
+    cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
+    cmodal:bind('', 'down', 'Move to Below Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
+    cmodal:bind('', 'space', 'Move to Next Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
+    cmodal:bind('', '[', 'Undo Window Manipulation', function() spoon.WinWin:undo() end)
+    cmodal:bind('', ']', 'Redo Window Manipulation', function() spoon.WinWin:redo() end)
+    cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
+
+    -- Register resizeM with modal supervisor
+    hsresizeM_keys = hsresizeM_keys or {"alt", "R"}
+    if string.len(hsresizeM_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
+            -- Deactivate some modal environments or not before activating a new one
+            spoon.ModalMgr:deactivateAll()
+            -- Show an status indicator so we know we're in some modal environment now
+            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- cheatsheetM modal environment (Because KSheet Spoon is NOT loaded, cheatsheetM will NOT be activated)
+if spoon.KSheet then
+    spoon.ModalMgr:new("cheatsheetM")
+    local cmodal = spoon.ModalMgr.modal_list["cheatsheetM"]
+    cmodal:bind('', 'escape', 'Deactivate cheatsheetM', function()
+        spoon.KSheet:hide()
+        spoon.ModalMgr:deactivate({"cheatsheetM"})
+    end)
+    cmodal:bind('', 'Q', 'Deactivate cheatsheetM', function()
+        spoon.KSheet:hide()
+        spoon.ModalMgr:deactivate({"cheatsheetM"})
+    end)
+
+    -- Register cheatsheetM with modal supervisor
+    hscheats_keys = hscheats_keys or {"alt", "S"}
+    if string.len(hscheats_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hscheats_keys[1], hscheats_keys[2], "Enter cheatsheetM Environment", function()
+            spoon.KSheet:show()
+            spoon.ModalMgr:deactivateAll()
+            spoon.ModalMgr:activate({"cheatsheetM"})
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- Register browser tab typist: Type URL of current tab of running browser in markdown format. i.e. [title](link)
+hstype_keys = hstype_keys or {"alt", "V"}
+if string.len(hstype_keys[2]) > 0 then
+    spoon.ModalMgr.supervisor:bind(hstype_keys[1], hstype_keys[2], "Type Browser Link", function()
+        local safari_running = hs.application.applicationsForBundleID("com.apple.Safari")
+        local chrome_running = hs.application.applicationsForBundleID("com.google.Chrome")
+        if #safari_running > 0 then
+            local stat, data = hs.applescript('tell application "Safari" to get {URL, name} of current tab of window 1')
+            if stat then hs.eventtap.keyStrokes("[" .. data[2] .. "](" .. data[1] .. ")") end
+        elseif #chrome_running > 0 then
+            local stat, data = hs.applescript('tell application "Google Chrome" to get {URL, title} of active tab of window 1')
+            if stat then hs.eventtap.keyStrokes("[" .. data[2] .. "](" .. data[1] .. ")") end
+        end
+    end)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Register Hammerspoon console
+hsconsole_keys = hsconsole_keys or {"alt", "Z"}
+if string.len(hsconsole_keys[2]) > 0 then
+    spoon.ModalMgr.supervisor:bind(hsconsole_keys[1], hsconsole_keys[2], "Toggle Hammerspoon Console", function() hs.toggleConsole() end)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Finally we initialize ModalMgr supervisor
+spoon.ModalMgr.supervisor:enter()
+
+----------------------------------------------------------------------------------------------------
 -- Auroreload config on save
 function reloadConfig(files)
     doReload = false
@@ -17,8 +316,10 @@ function reloadConfig(files)
     end
 end
 configWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+hs.alert.show("Hammerspoon config loaded")
+hs.sound.getByName(hs.sound.systemSounds()[5]):play()
 
+----------------------------------------------------------------------------------------------------
 -- Alert for new mail
 function mailAlert(files, flagTables)
     -- hs.alert.show(hs.inspect(flagTables))
@@ -32,13 +333,65 @@ fastmailWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Mail/fastmail/Inbox/
 athenaMailWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Mail/athenarc/Inbox/new/", mailAlert):start()
 uoaMailWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Mail/uoa/Inbox/new/", mailAlert):start()
 
--- Rearange windows to screens
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-    local laptopScreen = "Color LCD"
-    local dellScreen = "DELL U2719DC"
-    local windowLayout = {
-        {"Slack", nil, laptopScreen, hs.layout.maximized, nil, nil},
-        {"Firefox", nil, dellScreen, hs.layout.maximized, nil, nil},
-    }
-    hs.layout.apply(windowLayout)
-end)
+--- A closure function
+function open(name)
+    return function()
+        hs.application.launchOrFocus(name)
+        if name == 'Finder' then
+            hs.appfinder.appFromName(name):activate()
+        end
+    end
+end
+
+--- quick open applications
+hs.hotkey.bind({"alt ctrl"}, "A", open("Alacritty"))
+hs.hotkey.bind({"alt ctrl"}, "F", open("Firefox"))
+hs.hotkey.bind({"alt ctrl"}, "D", open("Dash"))
+hs.hotkey.bind({"alt ctrl"}, "J", open("Slack"))
+hs.hotkey.bind({"alt ctrl"}, "W", open("WhatsApp"))
+hs.hotkey.bind({"cmd"}, "E", open("Emacs"))
+
+--------------------------------
+-- START VIM CONFIG
+--------------------------------
+local VimMode = hs.loadSpoon("VimMode")
+local vim = VimMode:new()
+
+-- Configure apps you do *not* want Vim mode enabled in
+-- For example, you don't want this plugin overriding your control of Terminal
+-- vim
+vim
+  :disableForApp('Code')
+  :disableForApp('zoom.us')
+  :disableForApp('iTerm')
+  :disableForApp('iTerm2')
+  :disableForApp('Terminal')
+  :disableForApp('Alacritty')
+  :disableForApp('Emacs')
+
+-- If you want the screen to dim (a la Flux) when you enter normal mode
+-- flip this to true.
+vim:shouldDimScreenInNormalMode(false)
+
+-- If you want to show an on-screen alert when you enter normal mode, set
+-- this to true
+vim:shouldShowAlertInNormalMode(true)
+
+-- You can configure your on-screen alert font
+vim:setAlertFont("JetBrainsMono NF")
+
+-- Enter normal mode by typing a key sequence
+-- vim:enterWithSequence('jk')
+
+-- if you want to bind a single key to entering vim, remove the
+-- :enterWithSequence('jk') line above and uncomment the bindHotKeys line
+-- below:
+--
+-- To customize the hot key you want, see the mods and key parameters at:
+--   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+--
+vim:bindHotKeys({ enter = { {'ctrl'}, '[' } })
+
+--------------------------------
+-- END VIM CONFIG
+--------------------------------
